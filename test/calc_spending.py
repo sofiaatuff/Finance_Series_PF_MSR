a4a215f2ec756ebd9871635784993d9397ed82dd
sofiaatuff <sofiahtuff@gmail.com>
2023-06-07 16:44:31 -0700

    mathing the math

--- test/calc_spending.py
+++ test/calc_spending.py
@@ -27,6 +27,9 @@ condition = df['Card Member'] == "ST"
 st = df[condition]
 ct = df[-condition]
 
+#Lets create seperate DataFrames for each member to use later
+dfst = pd.DataFrame(st)
+sfct = pd.DataFrame(ct)
 
 # now we want to know how much each cardmember spent
 total_spent_st =st['Amount'].sum()
@@ -34,3 +37,18 @@ print("ST spent a total of: ", total_spent_st)
 
 total_spent_ct = ct["Amount"].sum()
 print("CT spent a total of: ", total_spent_ct)
+
+
+# now we want to know the top spent catagory
+top_spent = df.sort_values(by='Amount', ascending=False)
+top_5 = top_spent.head(5)
+print("The top 5 expensive purchases were: ")
+top_5
+
+#Now lets break it down by categories
+grouped_df = df.groupby('Category')
+
+for group_name, group_df in grouped_df: 
+    print(f"Group: {group_name}")
+    print(group_df)
+    print()
